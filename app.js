a81b243823418c92b27bfd36269b409fa362a8ed
Djotajo <20823564+Djotajo@users.noreply.github.com>
2025-05-08 22:25:21 +0200

    Install dependencies, establish db connection, create basic schema

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const path = require("node:path");
+const http = require("http");
+// const db = require("./db/queries");
+const { neon } = require("@neondatabase/serverless");
+const bcrypt = require("bcryptjs");
+const sql = neon(process.env.DATABASE_URL);
+const assetsPath = path.join(__dirname, "public");
+const PORT = process.env.PORT || 3000;
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+
+const indexRouter = require("./routes/indexRouter");
+// const signupRouter = require("./routes/signupRouter");
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
+
+// app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use("/", indexRouter);
+// app.use("/signup", signupRouter);
+// app.get("/welcome", (req, res) => res.render("welcome"));
+
+// Local Strategy - TOP
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const result = await db.getUser(username);
+      const user = result[0];
+      if (!user) {
+        console.log("no user");
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password_hashed);
+
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      console.log(err);
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const result = await db.getUserById(id);
+    const user = result[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+// Local Strategy - TOP
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+
+const indexRouter = Router();
+
+// const db = require("../db/queries");
+
+// const newMessageController = require("../controllers/newMessageController");
+// const newUserController = require("../controllers/newUserController");
+
+// async function getAllMembers(req, res) {
+//   const members = await db.getAllMembers();
+//   const member = members[0];
+//   return member;
+// }
+
+indexRouter.get("/", async (req, res) => {
+  // const messages = await db.getAllMessagesAndAuthors();
+  // res.render("index", { user: req.user, messages: messages });
+  res.render("index", { user: req.user });
+});
+
+// indexRouter.get("/logout", (req, res, next) => {
+//   req.logout((err) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     res.redirect("/");
+//   });
+// });
+
+// indexRouter.post("/membership", newUserController.newMemberValidate);
+
+// indexRouter.get("/member", async (req, res) => {
+//   res.render("member", { title: "Become a member", errors: [] });
+// });
+
+// indexRouter.post("/message", newMessageController.newMessageCreate);
+
+// indexRouter.post("/message/:id", newMessageController.deleteMessageById);
+
+module.exports = indexRouter;
